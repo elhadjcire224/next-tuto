5cd52512650fa683ad9b5e569d717cf43cad1684
elhadjcire224 <bahmamadoucire14@gmail.com>
2023-11-10 16:56:48 +0000

    tuto

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,6 @@
+export default function Page(){
+    
+    return (
+        <div>ici c'est custommers</div>
+    )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Page(){
+
+    return(
+        <div>
+            bonjour tu est chez dash
+        </div>
+    )
+}
